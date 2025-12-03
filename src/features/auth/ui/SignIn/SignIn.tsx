c4f4be14c3c936642894e808125d6c2e0d833b8e
OverTheTop404 <over_the_top_404@mail.ru>
2025-12-03 21:05:26 +0400

    Test auto push

--- src/features/auth/ui/SignIn/SignIn.tsx
+++ src/features/auth/ui/SignIn/SignIn.tsx
@@ -129,7 +129,9 @@ export const SignIn = () => {
       </LoginForm>
       <LoginInfo>
         <FormLabel>
-          <p style={{ marginBottom: '15px', fontSize: '17px', fontWeight: '600' }}>Guest access if you just want to watch:</p>
+          <p style={{ marginBottom: '15px', fontSize: '17px', fontWeight: '600' }}>
+            Guest access if you just want to watch: ////////////////////////////////////////////////////
+          </p>
           <p>
             <b>Email:</b> free@samuraijs.com
           </p>
